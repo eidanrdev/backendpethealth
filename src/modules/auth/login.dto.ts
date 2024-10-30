1599dac10e402647d3cd2d6f5b0c7514daccc01a
eidanrdev <e05172004@gmail.com>
2024-10-30 10:37:13 -0600

    Update login.dto.ts

--- src/modules/auth/login.dto.ts
+++ src/modules/auth/login.dto.ts
@@ -2,10 +2,6 @@ import { IsString, IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 import { Role } from './role.enum'; 
 
 export class LoginDto {
-  @IsString({ message: 'El nombre debe ser una cadena de caracteres' })
-  @IsNotEmpty({ message: 'El nombre es obligatoria' })
-  name: string;
-  
   @IsEmail({}, { message: 'El correo electrónico es inválido' })
   @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
   email: string;
